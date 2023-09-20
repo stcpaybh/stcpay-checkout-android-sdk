cb9a732612e84b55096938ea14c0fdaf9ade2779
Mian Sarim Hameed <miansarimhameed@gmail.com>
2023-09-20 15:53:50 +0500

    Implement Exception Handling on openStcPayApp() :goal_net:

--- stcPayCheckout/src/main/java/com/stcpay/checkout/utils/StcPayCheckoutHelper.kt
+++ stcPayCheckout/src/main/java/com/stcpay/checkout/utils/StcPayCheckoutHelper.kt
@@ -5,14 +5,15 @@ import java.security.InvalidKeyException
 import java.security.NoSuchAlgorithmException
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+import kotlin.text.Charsets.UTF_8
 
 fun getHashedData(secretKey: String, data: String): String {
     try {
         val sha512HMAC = Mac.getInstance(HMAC_SHA_512)
-        val secretKeySpec = SecretKeySpec(secretKey.toByteArray(), HMAC_SHA_512)
+        val secretKeySpec = SecretKeySpec(secretKey.toByteArray(UTF_8), HMAC_SHA_512)
         sha512HMAC.init(secretKeySpec)
-        val bytes = sha512HMAC.doFinal(data.toByteArray())
-        return Base64.encodeToString(bytes, 2)
+        val bytes = sha512HMAC.doFinal(data.toByteArray(UTF_8))
+        return Base64.encodeToString(bytes, Base64.NO_WRAP)
     } catch (e: NoSuchAlgorithmException) {
         e.printStackTrace()
     } catch (e: InvalidKeyException) {
