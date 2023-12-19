ac0c191dd958cd743d5bb13db1caee8e3958bf1e
Haider Saddique <hsaddique@sadadbahrain.com>
2023-12-19 12:37:32 +0300

    Version bump

--- stcPayCheckout/build.gradle.kts
+++ stcPayCheckout/build.gradle.kts
@@ -53,7 +53,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.stcpaybh"
             artifactId = "stcpay-checkout-android-sdk"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
