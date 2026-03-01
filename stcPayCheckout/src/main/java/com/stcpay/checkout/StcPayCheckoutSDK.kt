0824b752b969fca888c5478bc73e62f524955d5e
osamaanwar <osamabhatti774@gmail.com>
2026-03-01 15:00:04 +0500

    flag updates

--- stcPayCheckout/src/main/java/com/stcpay/checkout/StcPayCheckoutSDK.kt
+++ stcPayCheckout/src/main/java/com/stcpay/checkout/StcPayCheckoutSDK.kt
@@ -68,7 +68,10 @@ object StcPayCheckoutSDK {
 
         if (intent != null) {
             intent.apply {
-                flags = Intent.FLAG_INCLUDE_STOPPED_PACKAGES
+                flags =
+                    Intent.FLAG_ACTIVITY_NEW_TASK or
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                            Intent.FLAG_INCLUDE_STOPPED_PACKAGES
                 putExtra(MERCHANT_ID, stcPayCheckoutSDKConfiguration.merchantId)
                 putExtra(EXTERNAL_REF_ID, stcPayCheckoutSDKConfiguration.externalRefId)
                 putExtra(AMOUNT, stcPayCheckoutSDKConfiguration.amount)
